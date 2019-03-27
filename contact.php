18f10e40a8794d4020cb276bd180c5d52b866944
annettethomas <42778997+annettethomas@users.noreply.github.com>
2019-03-27 10:47:23 -0600

    added message

--- contact.php
+++ contact.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
-    <?php 
+    <?php
       $name = $_POST['name'];
       $email = $_POST['email'];
       $subject = $_POST['subject'];
       $message = $_POST['message'];
       $recipient = "annettethomasmarie@gmail.com";
       $mailheader = "From:  $email \r\n";
-      mail($recipient, $subject, $message, $mailheader) or die("Error:");
+      mail($recipient, $subject, $message, $mailheader, $message) or die("Error:");
       echo "Thank you! I'll be in touch soon!"
       ?>
 </body>
